3b79f37ccf664b687bf02dacf1fd9de9920cc22a
unknown <pmr2@k106-7.student.staffs.ac.uk>
2014-10-01 11:23:48 +0100

    [+] Added Test.cs [PR]

---
+++ gitTest/gitTest/Test.cs
@@ -0,0 +1,20 @@
+using System;
+
+namespace gitTest
+{
+	public class Test
+	{
+		public int num1 = 0;
+		
+		public int Num1
+		{
+			get{return num1;}
+			set{num1 = value;}
+		}
+		
+		public Test ()
+		{
+		}
+	}
+}
+
